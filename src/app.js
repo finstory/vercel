2f4ff051e97eeba6e0a16a58808e07ee1104f393
finstory <facu995electro@hotmail.com>
2023-05-04 03:41:15 -0300

    first

--- src/app.js
+++ src/app.js
@@ -8,23 +8,23 @@ const morgan = require('morgan');
 // require('./db.js');
 
 const server = express();
-// const cors = require('cors');
-// server.use(cors());
+const cors = require('cors');
+server.use(cors());
 
-// server.name = 'API';
+server.name = 'API';
 
-// server.use(bodyParser.urlencoded({ extended: true, limit: '1000mb' }));
-// server.use(bodyParser.json({ limit: '1000mb' }));
-// server.use(cookieParser());
+server.use(bodyParser.urlencoded({ extended: true, limit: '1000mb' }));
+server.use(bodyParser.json({ limit: '1000mb' }));
+server.use(cookieParser());
 // *DEBUG
-// server.use(morgan('dev'));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, PATCH, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, PATCH, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 
 
