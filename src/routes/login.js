331327ff8b8749e027fea5e05ec4f60451446e0f
finstory <facu995electro@hotmail.com>
2023-05-04 07:07:49 -0300

    first

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,8 +4,8 @@ const { User } = require("../db");
 router.get("/login", async function (req, res) {
     try {
         const { username } = req.query;
-         const usersDB = await User.findAll();
-        res.status(200).json({ msg: "2222you logged in successfully." });
+        const usersDB = await User.findAll();
+        res.status(200).json(usersDB);
 
     } catch (error) {
         res.status(404).json(error.message);
