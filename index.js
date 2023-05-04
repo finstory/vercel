07c48f7f95d26f821955b3502ac688ab039a2ab1
finstory <facu995electro@hotmail.com>
2023-05-04 03:35:16 -0300

    first commit

--- index.js
+++ index.js
@@ -18,12 +18,17 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+// const { conn } = require('./src/db.js');
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+// conn.sync({ force: false }).then(() => {
+//   server.listen(3001, () => {
+//     console.log('%s listening at 3001'); // eslint-disable-line no-console
+//   });
+// });
+
+
+server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
 });
